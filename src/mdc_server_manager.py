06f3e467da6b1ad76ff8d5f63119796aafc477d2
giuppe3d <giuppe3d>
2008-02-17 18:50:00 +0000

    add scheduling of descriptors sending

--- src/mdc_server_manager.py
+++ src/mdc_server_manager.py
@@ -2,6 +2,8 @@
 import socket, struct, time
 import threading
 
+import globals
+
 
 class MdcServerManager ( threading.Thread ):
 
@@ -10,21 +12,29 @@ class MdcServerManager ( threading.Thread ):
         self.buffer = ""
         self.client = socket.socket ( socket.AF_INET, socket.SOCK_DGRAM )
         self.client.connect ( (host, 5552)  )
+        self.descriptors_schedule = list()
         
     def run ( self ):
       
+
+        global g_sequences_cache
         
         while(1):
-            # Send some messages:
+            # this should prevent CPU chocking
             time.sleep(1)
+            
             if(len(self.buffer) !=0):
                 sent = self.client.send ( self.buffer )
                 self.buffer = self.buffer[sent:] 
-                
+            else:
+                curr_desc = self.descriptors_schedule.pop()
+                self.buffer += globals.g_sequences_cache.get_descriptor(curr_desc[0], curr_desc[1], curr_desc[2])  
         
     def close(self):
         # Close the connection
         self.client.close()
         
-    def send_desc(self, descriptor):
-        self.buffer += descriptor
+    def send_desc(self, stream_id, description_id, seq_start, seq_end):
+        for index in range(seq_start, seq_end+1):
+            self.descriptors_schedule.insert(0, (stream_id, description_id, index))
+        
